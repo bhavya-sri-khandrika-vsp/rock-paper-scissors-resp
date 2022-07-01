b7692d0f88bada7fe0fe8d7511507671e3574f13
bhavya-sri-khandrika-vsp <kvspbs@gmail.com>
2022-07-01 10:59:42 +0530

    'commit1'

---
+++ Rock Paper Scissors/index.js
@@ -0,0 +1,18 @@
+//windows redirection JS code
+function goto()
+{
+    window.location.href = "game.html";
+}
+
+//displaying image on user input
+
+
+
+
+
+
+
+function disp(y){
+var x = document.getElementsByClassName("cont1");
+x.src = y;
+}
